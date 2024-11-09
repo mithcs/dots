26c1a99b8cf47732a3ff4fd80439ff8345440724
Mitesh Soni <smiteshhc@gmail.com>
2024-11-09 20:39:29 +0530

    awesomewm: Added autostart commands

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -67,18 +67,7 @@ local function run_once(cmd_arr)
     end
 end
 
-run_once({ "urxvtd", "unclutter -root" }) -- comma-separated entries
-
--- This function implements the XDG autostart specification
---[[
-awful.spawn.with_shell(
-    'if (xrdb -query | grep -q "^awesome\\.started:\\s*true$"); then exit; fi;' ..
-    'xrdb -merge <<< "awesome.started:true";' ..
-    -- list each of your autostart commands, followed by ; inside single quotes, followed by ..
-    'dex --environment Awesome --autostart --search-paths ' ..
-    '"${XDG_CONFIG_HOME:-$HOME/.config}/autostart:${XDG_CONFIG_DIRS:-/etc/xdg}/autostart";' -- https://github.com/jceb/dex
-)
---]]
+run_once({ "nvidia-settings --load-config-only", "odomodoro-daemon" })
 
 -- }}}
 
