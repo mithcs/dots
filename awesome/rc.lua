38f8b2cd1a1a08e997f2c0912196d539b4e3c001
Mitesh Soni <smiteshhc@gmail.com>
2024-11-09 21:49:54 +0530

    awesomewm: Fixed mouse bindings on scroll

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -210,8 +210,8 @@ awful.screen.connect_for_each_screen(function(s) beautiful.at_screen_connect(s) 
 -- {{{ Mouse bindings
 
 root.buttons(mytable.join(
-    awful.button({ }, 4, awful.tag.viewnext),
-    awful.button({ }, 5, awful.tag.viewprev)
+    awful.button({ }, 4, awful.tag.viewprev),
+    awful.button({ }, 5, awful.tag.viewnext)
 ))
 
 -- }}}
