b26234fa920003e1ec4f40ef3acc1fe0023adb9c
Mitesh Soni <smiteshhc@gmail.com>
2024-11-15 01:38:56 +0530

    awesomewm: Changed layout order

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -120,8 +120,9 @@ awful.util.terminal = terminal
 awful.util.tagnames = { "1", "2", "3", "4", "5" }
 awful.layout.layouts = {
     awful.layout.suit.tile,
-    awful.layout.suit.spiral,
     awful.layout.suit.floating,
+    awful.layout.suit.corner.nw,
+    awful.layout.suit.spiral,
     awful.layout.suit.tile.left,
     awful.layout.suit.tile.bottom,
     awful.layout.suit.tile.top,
@@ -131,7 +132,6 @@ awful.layout.layouts = {
     awful.layout.suit.max,
     awful.layout.suit.max.fullscreen,
     awful.layout.suit.magnifier,
-    awful.layout.suit.corner.nw,
     -- awful.layout.suit.corner.ne,
     -- awful.layout.suit.corner.sw,
     -- awful.layout.suit.corner.se,
