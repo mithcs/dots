3d82a53138b5a199f6a660f254585bc85abe532c
Mitesh Soni <smiteshhc@gmail.com>
2024-12-14 14:50:36 +0530

    st: Changed colorscheme to catppuccin-latte

--- st/config.h
+++ st/config.h
@@ -97,32 +97,32 @@ unsigned int tabspaces = 8;
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
 	/* 8 normal colors */
-	"black",
-	"red3",
-	"green3",
-	"yellow3",
-	"blue2",
-	"magenta3",
-	"cyan3",
-	"gray90",
+	"#dc8a78", /* rosewater */
+	"#d20f39", /* red */
+	"#40a02b", /* green */
+	"#df8e1d", /* yellow */
+	"#1e66f5", /* blue */
+	"#ea76cb", /* mauve */
+	"#179299", /* teal */
+	"#4c4f69", /* base */
 
 	/* 8 bright colors */
-	"gray50",
-	"red",
-	"green",
-	"yellow",
-	"#5c5cff",
-	"magenta",
-	"cyan",
-	"white",
+	"#acb0be", /* overlay2 */
+	"#fe640b", /* maroon */
+	"#40a02b", /* green */
+	"#df8e1d", /* yellow */
+	"#1e66f5", /* blue */
+	"#8839ef", /* purple */
+	"#04a5e5", /* sapphire */
+	"#7287fd", /* text */
 
 	[255] = 0,
 
 	/* more colors can be added after 255 to use with DefaultXX */
-	"#cccccc",
-	"#555555",
-	"gray90", /* default foreground colour */
-	"black", /* default background colour */
+	"#eff1f5", /* mantle */
+	"#4c4f69", /* crust */
+	"#7287fd", /* default foreground colour (text) */
+	"#eff1f5", /* default background colour (mantle) */
 };
 
 
@@ -132,7 +132,7 @@ static const char *colorname[] = {
  */
 unsigned int defaultfg = 258;
 unsigned int defaultbg = 259;
-unsigned int defaultcs = 256;
+unsigned int defaultcs = 258;
 static unsigned int defaultrcs = 257;
 
 /*
